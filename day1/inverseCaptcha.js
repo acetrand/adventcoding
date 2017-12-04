ed385100615819f2d1c9b6c27498575673c5686f
Fredrik Åstrand <acetrand+dev@gmail.com>
2017-12-04 11:47:56 +0100

    feat(): add number when neighbors are the same

--- day1/inverseCaptcha.js
+++ day1/inverseCaptcha.js
@@ -1,10 +1,22 @@
 inverseCaptcha = (input) => {
     const numbers = getArrayOfNumbers(input);
     let answer = 0;
+    
+    numbers.forEach((num, index) => {
+        const nextNum = isLast(index) ? numbers[0] : numbers[index + 1];
+		
+		if(num === nextNum) {
+			answer += Number(num);
+		}
+    });
 
     return answer;
     
     function getArrayOfNumbers(numbers) {
         return ("" + numbers).split('')
     }
+	
+	function isLast(index) {
+		return index === numbers.length - 1;
+	}
 }
