d71fd124e88391674f2a5e2452cf7434cc65e7f5
Fredrik Åstrand <acetrand+dev@gmail.com>
2017-12-04 12:26:18 +0100

    feat(): safety check that current num is a number

--- day1/inverseCaptcha.js
+++ day1/inverseCaptcha.js
@@ -3,16 +3,19 @@ inverseCaptcha = (stringOfNumbers) => {
     let answer = 0;
     
     numbers.forEach((num, index) => {
+        if(!Number(num)) {
+            throw(num + ' is not a number');
+        }
         const nextNum = isLast(index) ? numbers[0] : numbers[index + 1];
 		
-		if(num === nextNum) {
-			answer += Number(num);
-		}
+        if(num === nextNum) {
+            answer += Number(num);
+        }
     });
 
     return answer;
     	
-	function isLast(index) {
-		return index === numbers.length - 1;
-	}
+    function isLast(index) {
+        return index === numbers.length - 1;
+    }
 }
